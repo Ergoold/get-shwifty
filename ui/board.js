c32da7cc58b767265cb9a6e277d52755982faeb5
Ergoold <eran1gold@gmail.com>
2022-07-03 19:12:41 +0300

    Make renderBoard overwrite board instead of append

--- ui/board.js
+++ ui/board.js
@@ -1,5 +1,6 @@
 export function renderBoard(board) {
     let table = document.getElementById('board');
+    table.innerHTML = '';
     for (let row of board.cells) {
         let tr = document.createElement('tr');
         for (let cell of row) {
